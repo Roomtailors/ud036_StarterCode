c7493cac44de6287ea1e225598284429cd3e843d
Tobias <tobias@roomtailors.de>
2016-05-30 11:30:22 +0200

    Movie Project

---
+++ media.py
@@ -0,0 +1,20 @@
+import webbrowser
+
+class Movie():
+    """Class for all movie objects to store data
+
+    Attributes:
+        movie_title (str): Title of the movie
+        movie_storyline (str): Short description of the movie (1 paragraph)
+        poster_image (str): URL of the movie poster image
+        trailer_youtube (str): URL to the movie trailer
+    """
+
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Udacity Project 1: Movie Trailer Website
+
+Shows a list of 6 favorite movies. Allows to play the movie trailers.
+
+## Getting Started
+
+GIT CLONE
+
+### Prerequisities
+
+Python standard library.
+
+### Installing
+
+Copy all files. Run a Python server and start entertainment_center.py
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md] file for details
